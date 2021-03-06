2c9c4fe555241dad686ec30fe46a36be376529d6
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-06 10:28:09 +0100

    v2.2.0

--- src/Tether.tsx
+++ src/Tether.tsx
@@ -1,16 +1,21 @@
+import { EventTypes, initialState, reducer } from './State';
 import React, { ReactNode } from 'react';
+
 import ContextProvider from './ContextProvider';
-import { initialState, reducer } from './State';
 
 interface Props {
   children: ReactNode;
   url: string;
+  scopes?: EventTypes[];
 }
 
-const Tether: React.FC<Props> = ({ children, url }) => (
-  <ContextProvider initialState={initialState} reducer={reducer} url={url}>
-    {children}
-  </ContextProvider>
-);
+const Tether: React.FC<Props> = ({ children, url, scopes }) => {
+  const query = scopes ? '?scopes=' + scopes.join('&scopes=') : '';
+  return (
+    <ContextProvider initialState={initialState} reducer={reducer} url={url + query}>
+      {children}
+    </ContextProvider>
+  );
+};
 
 export default Tether;
